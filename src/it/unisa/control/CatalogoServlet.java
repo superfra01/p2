2378298123c67d7f10a66efc5c6da6d691b7aa2f
superfra01 <151184247+superfra01@users.noreply.github.com>
2024-06-05 17:06:14 +0200

    id3-resolved

--- src/it/unisa/control/CatalogoServlet.java
+++ src/it/unisa/control/CatalogoServlet.java
@@ -2,10 +2,6 @@ package it.unisa.control;
 
 import java.io.IOException; 
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,76 +13,79 @@ import it.unisa.model.ProdottoDao;
 
 @WebServlet("/catalogo")
 public class CatalogoServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		ProdottoDao prodDao = new ProdottoDao();
-		ProdottoBean bean = new ProdottoBean();
-		String sort = request.getParameter("sort");
-		String action = request.getParameter("action");
-		String redirectedPage = request.getParameter("page");;
-	
-		try {
-			if(action!=null) {
-				if(action.equalsIgnoreCase("add")) {
-					bean.setNome(request.getParameter("nome"));
-					bean.setDescrizione(request.getParameter("descrizione"));
-					bean.setIva(request.getParameter("iva"));
-					bean.setPrezzo(Double.parseDouble(request.getParameter("prezzo")));
-					bean.setQuantità(Integer.parseInt(request.getParameter("quantità")));
-					bean.setPiattaforma(request.getParameter("piattaforma"));
-					bean.setGenere(request.getParameter("genere"));
-					bean.setImmagine(request.getParameter("img"));
-					bean.setDataUscita(request.getParameter("dataUscita"));
-					bean.setDescrizioneDettagliata(request.getParameter("descDett"));
-					bean.setInVendita(true);
-					prodDao.doSave(bean);
-				}
-				
-				else if(action.equalsIgnoreCase("modifica")) {
-					
-					bean.setIdProdotto(Integer.parseInt(request.getParameter("id")));
-					bean.setNome(request.getParameter("nome"));
-					bean.setDescrizione(request.getParameter("descrizione"));
-					bean.setIva(request.getParameter("iva"));
-					bean.setPrezzo(Double.parseDouble(request.getParameter("prezzo")));
-					bean.setQuantità(Integer.parseInt(request.getParameter("quantità")));
-					bean.setPiattaforma(request.getParameter("piattaforma"));
-					bean.setGenere(request.getParameter("genere"));
-					bean.setImmagine(request.getParameter("img"));
-					bean.setDataUscita(request.getParameter("dataUscita"));
-					bean.setDescrizioneDettagliata(request.getParameter("descDett"));
-					bean.setInVendita(true);
-					prodDao.doUpdate(bean);	
-				}
+        ProdottoDao prodDao = new ProdottoDao();
+        ProdottoBean bean = new ProdottoBean();
+        String sort = request.getParameter("sort");
+        String action = request.getParameter("action");
+        String redirectedPage = request.getParameter("page");
 
-				request.getSession().removeAttribute("categorie");
+        try {
+            if (action != null) {
+                if (action.equalsIgnoreCase("add")) {
+                    bean.setNome(sanitizeInput(request.getParameter("nome")));
+                    bean.setDescrizione(sanitizeInput(request.getParameter("descrizione")));
+                    bean.setIva(sanitizeInput(request.getParameter("iva")));
+                    bean.setPrezzo(Double.parseDouble(request.getParameter("prezzo")));
+                    bean.setQuantità(Integer.parseInt(request.getParameter("quantità")));
+                    bean.setPiattaforma(sanitizeInput(request.getParameter("piattaforma")));
+                    bean.setGenere(sanitizeInput(request.getParameter("genere")));
+                    bean.setImmagine(sanitizeInput(request.getParameter("img")));
+                    bean.setDataUscita(sanitizeInput(request.getParameter("dataUscita")));
+                    bean.setDescrizioneDettagliata(sanitizeInput(request.getParameter("descDett")));
+                    bean.setInVendita(true);
+                    prodDao.doSave(bean);
+                } else if (action.equalsIgnoreCase("modifica")) {
+                    bean.setIdProdotto(Integer.parseInt(request.getParameter("id")));
+                    bean.setNome(sanitizeInput(request.getParameter("nome")));
+                    bean.setDescrizione(sanitizeInput(request.getParameter("descrizione")));
+                    bean.setIva(sanitizeInput(request.getParameter("iva")));
+                    bean.setPrezzo(Double.parseDouble(request.getParameter("prezzo")));
+                    bean.setQuantità(Integer.parseInt(request.getParameter("quantità")));
+                    bean.setPiattaforma(sanitizeInput(request.getParameter("piattaforma")));
+                    bean.setGenere(sanitizeInput(request.getParameter("genere")));
+                    bean.setImmagine(sanitizeInput(request.getParameter("img")));
+                    bean.setDataUscita(sanitizeInput(request.getParameter("dataUscita")));
+                    bean.setDescrizioneDettagliata(sanitizeInput(request.getParameter("descDett")));
+                    bean.setInVendita(true);
+                    prodDao.doUpdate(bean);
+                }
+                request.getSession().removeAttribute("categorie");
+            }
 
-			}
-			
-		} catch (SQLException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
+        } catch (SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
 
+        try {
+            request.getSession().removeAttribute("products");
+            request.getSession().setAttribute("products", prodDao.doRetrieveAll(sort));
+        } catch (SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
 
-		try {
-			request.getSession().removeAttribute("products");
-			request.getSession().setAttribute("products", prodDao.doRetrieveAll(sort));
-		} catch (SQLException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
-		
-			
-			response.sendRedirect(request.getContextPath() + "/" +redirectedPage);
-		
-		
-	}
+        response.sendRedirect(request.getContextPath() + "/" + redirectedPage);
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 
+    private String sanitizeInput(String input) {
+        if (input != null) {
+            // Replace HTML special characters with their respective HTML entities
+            input = input.replace("&", "&amp;");
+            input = input.replace("<", "&lt;");
+            input = input.replace(">", "&gt;");
+            input = input.replace("\"", "&quot;");
+            input = input.replace("'", "&#x27;");
+            input = input.replace("/", "&#x2F;");
+        }
+        return input;
+    }
 }
